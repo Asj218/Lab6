1a2a6dc04618ef26b7fcf2c4910df2df36968cf5
Анастасия <azizovaa031@gmail.com>
2024-11-21 20:04:40 +0700

    Начало ViewHolder (CrimeListFragment.kt)

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -40,6 +40,10 @@ class CrimeListFragment : Fragment() {
         return view
     }
 
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+    }
+
     companion object {
         fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
