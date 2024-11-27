86167d544e5a66c931009919e67816390e786572
Анастасия <azizovaa031@gmail.com>
2024-11-27 23:08:59 +0700

    Извлечение даты и инициализация DatePickerDialog (DatePickerFragment.kt)

--- app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/example/criminalintent/DatePickerFragment.kt
@@ -4,13 +4,16 @@ import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import java.sql.Date
 import java.util.Calendar
 
 private const val ARG_DATE = "date"
 
 class DatePickerFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val date = arguments?.getSerializable(ARG_DATE) as Date
         val calendar = Calendar.getInstance()
+        calendar.time = date
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
